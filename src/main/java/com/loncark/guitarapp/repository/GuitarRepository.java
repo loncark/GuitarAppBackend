708180cfafc05f5cde4ff96c3304be58e04b9f25
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2023-05-31 13:01:00 +0200

    GET all returns all guitars on localhost:7000.

--- src/main/java/com/loncark/guitarapp/repository/GuitarRepository.java
+++ src/main/java/com/loncark/guitarapp/repository/GuitarRepository.java
@@ -2,9 +2,11 @@ package com.loncark.guitarapp.repository;
 
 import com.loncark.guitarapp.model.Guitar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
+@Transactional
 public interface GuitarRepository extends JpaRepository<Guitar, Long> {
 
 }
