7061aaf4953e8910cf3e4d892d16aa5fb12fe95c
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2023-06-03 09:20:52 +0200

    Wrote POST and PUT tests

--- src/test/java/com/loncark/guitarapp/GuitarControllerTest.java
+++ src/test/java/com/loncark/guitarapp/GuitarControllerTest.java
@@ -44,4 +44,49 @@ public class GuitarControllerTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
+
+    @Test
+    public void testSave() throws Exception {
+        String requestBody = "{\"code\": \"1239\", " +
+                "\"body\": \"Mahogany\", " +
+                "\"neck\": \"Maple\", " +
+                "\"price\": 129.00, " +
+                "\"stock\": 5, " +
+                "\"name\": \"Squier Classic Vibe Telecaster\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Squier Classic Vibe Telecaster"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(129.00))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.stock").value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1239));
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        String requestBody = "{" +
+                "\"id\": \"3\", " +
+                "\"code\": \"1111\", " +
+                "\"body\": \"Mahogany\", " +
+                "\"neck\": \"Maple\", " +
+                "\"price\": 129.00, " +
+                "\"stock\": 5, " +
+                "\"name\": \"Squier Classic Vibe Telecaster\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Squier Classic Vibe Telecaster"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(129.00))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.stock").value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1111))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3));
+    }
 }
