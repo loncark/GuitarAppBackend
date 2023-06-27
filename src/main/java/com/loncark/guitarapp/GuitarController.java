c5cb622250426802f4f5bee2fb955f70e7b45bfd
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2023-06-27 17:22:13 +0200

    Configured basic Prometheus monitoring.

--- src/main/java/com/loncark/guitarapp/GuitarController.java
+++ src/main/java/com/loncark/guitarapp/GuitarController.java
@@ -10,6 +10,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+import static java.lang.Thread.sleep;
+
 @RestController
 @RequestMapping("/")
 public class GuitarController {
